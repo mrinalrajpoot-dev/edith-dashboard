5c3848853b3ef87dda92b9effc318a8cbff40ac1
Mrinal Rajpoot <mrinal.rajpoot.dev@gmail.com>
2025-06-15 11:57:54 +0530

    Added default task data for EDITH

---
+++ src/data/tasks.js
@@ -0,0 +1,7 @@
+const defaultTasks = [
+  { name: "Finish dashboard upgrade", done: false },
+  { name: "Apply to 3 jobs today", done: false },
+  { name: "Practice interview Qs", done: false }
+];
+
+export default defaultTasks;
